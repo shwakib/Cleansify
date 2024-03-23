2309ea69a67b5a1583cd4bdc6557b0814886067b
Fahmid015 <shabab015@gmail.com>
2024-03-23 12:45:43 +1100

    FEAT: Added router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import AuthPage from './pages/AuthPage'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-   
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
