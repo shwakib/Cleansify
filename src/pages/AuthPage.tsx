f5f856faf80dcc10f93bbc2ccef3f9a524be2020
Fahmid015 <shabab015@gmail.com>
2024-03-23 14:06:13 +1100

    FEAT: Added AuthPage

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,7 +1,44 @@
+import { Button, Grid, Typography, styled } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const StyledContainer = styled(Grid)(() => ({
+  alignItems: 'center',
+  textAlign: 'center'
+}))
 
 const AuthPage = () => {
-  return <div>AuthPage</div>
+  const navigate = useNavigate()
+
+  return (
+    <StyledContainer container md={6} xs={12} gap={5}>
+      <Grid item xs={12}>
+        <Typography variant="h2" fontWeight={'bold'}>
+          Cleansify
+        </Typography>
+      </Grid>
+      <Grid item container gap={2}>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+            onClick={() => {
+              navigate('/signup')
+            }}
+          >
+            Sign up
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="outlined" color="primary" size="large" fullWidth>
+            Login
+          </Button>
+        </Grid>
+      </Grid>
+    </StyledContainer>
+  )
 }
 
 export default AuthPage
