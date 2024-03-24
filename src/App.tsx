ad87b71fee540a2fe148efefd49b26bad5d538dd
Fahmid015 <shabab015@gmail.com>
2024-03-24 20:01:28 +1100

    FEAT: Added user context

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,22 @@ import theme from './styles/theme'
 import AppContainer from './layout/Container'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import UserProvider from './state/user/user.provider'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <AppContainer>
-        <Router>
-          <Routes>
-            <Route path="/" element={<AuthPage />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Router>
-      </AppContainer>
+      <UserProvider>
+        <AppContainer>
+          <Router>
+            <Routes>
+              <Route path="/" element={<AuthPage />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </Router>
+        </AppContainer>
+      </UserProvider>
     </ThemeProvider>
   )
 }
