f18496dab9ba5cbf3d007a29223933db6131a5c4
Fahmid015 <shabab015@gmail.com>
2024-03-30 15:49:37 +1100

    FEAT: DataDoc interface

---
+++ src/models/data.model.ts
@@ -0,0 +1,21 @@
+import { DocumentData } from 'firebase/firestore'
+import { FormData } from '../components/DataForm'
+
+export interface DataDoc
+  extends Omit<
+    FormData,
+    'electricityBill' | 'gasBill' | 'fuelBill' | 'carBill'
+  > {
+  userId: string
+  accountType: string
+  carbonFootPrint: string
+  date: string
+  electricityBillFilePath: string
+  gasBillFilePath: string
+  fuelBillFilePath: string
+  carBillFilePath: string
+}
+
+export interface DataTable {
+  [year: string]: DocumentData[]
+}
